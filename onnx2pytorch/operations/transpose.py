5454adbbde5d0e89ce7a1f195248e13b7491214f
cw3344 <cw3344@drexel.edu@xulab001.cci.drexel.edu>
2023-06-15 16:57:40 +0000

    add comment in transpose.py

--- onnx2pytorch/operations/transpose.py
+++ onnx2pytorch/operations/transpose.py
@@ -12,6 +12,7 @@ class Transpose(nn.Module):
             dims = tuple(reversed(range(data.dim())))
         else:
             dims = self.dims
+        # if the first dim is batch size, manually add the batch size to the shape
         if len(data.shape)==len(dims)+1:
             dims = tuple([0]+[tmp+1 for tmp in dims])
         transposed = data.permute(dims)
